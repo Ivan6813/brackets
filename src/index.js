cd6188ea81ae86878ccc62264018b52446b8ee14
Ivan6813 <bmwlord750@gmail.com>
2021-03-15 17:52:40 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let br;
+  let com;
+  let counter;
+
+  while (counter !== bracketsConfig.length) {
+    counter = 0;
+
+    for (i = 0; i < bracketsConfig.length; i++) {
+      br = bracketsConfig[i][0] + bracketsConfig[i][1];
+      com = str.replace(br, "");
+      if (str === com) {
+        counter++;
+      }else{
+        str = com;
+      }
+    }
+  }
+
+  return (str.length === 0) ? true : false;
 }
